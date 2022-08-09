836b684f8f9d9914484b192faeaffaf0359dcfb6
ozarskiw <ozarskiw@gmail.com>
2022-08-09 13:03:48 +0200

    Training history date picker paging.

--- app/src/main/java/com/example/treadmillassistant/backend/training/TrainingCalendar.kt
+++ app/src/main/java/com/example/treadmillassistant/backend/training/TrainingCalendar.kt
@@ -40,6 +40,35 @@ class TrainingCalendar(var currentDate: Date = Date(), var trainingList: Mutable
         trainingList.sortBy{ it.trainingTime }
     }
 
+    fun getHistoryTrainingsForMonth(calendar: Calendar): MutableList<Training>{
+
+        calendar.set(Calendar.DAY_OF_MONTH, calendar.getActualMinimum(Calendar.DAY_OF_MONTH))
+        var from = Calendar.getInstance()
+        from.set(Calendar.MONTH, calendar.get(Calendar.MONTH))
+        from.set(Calendar.YEAR, calendar.get(Calendar.YEAR))
+        from.set(Calendar.DAY_OF_MONTH, calendar.get(Calendar.DAY_OF_MONTH))
+
+        calendar.set(Calendar.DAY_OF_MONTH, calendar.getActualMaximum(Calendar.DAY_OF_MONTH))
+        var to = Calendar.getInstance()
+        to.set(Calendar.MONTH, calendar.get(Calendar.MONTH))
+        to.set(Calendar.YEAR, calendar.get(Calendar.YEAR))
+        to.set(Calendar.DAY_OF_MONTH, calendar.get(Calendar.DAY_OF_MONTH))
+
+        from.set(Calendar.HOUR_OF_DAY, 0)
+        from.set(Calendar.MINUTE, 0)
+        to.set(Calendar.HOUR_OF_DAY, 23)
+        to.set(Calendar.MINUTE, 59)
+
+        var trainingList = trainingList.filter {
+            it.trainingTime.time>from.time
+            && it.trainingTime.time<to.time
+            && (it.trainingStatus == TrainingStatus.Finished  || it.trainingStatus == TrainingStatus.Abandoned )
+        }.toMutableList()
+        trainingList.sortByDescending{ it.trainingTime }
+
+        return trainingList
+    }
+
     fun getNewID(): Long {
         return if (user.trainingSchedule.trainingList.isEmpty()) {
             1L
--- app/src/main/java/com/example/treadmillassistant/ui/trainingHistory/TrainingHistoryFragment.kt
+++ app/src/main/java/com/example/treadmillassistant/ui/trainingHistory/TrainingHistoryFragment.kt
@@ -1,19 +1,31 @@
 package com.example.treadmillassistant.ui.trainingHistory
 
+import android.content.res.Resources
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.NumberPicker
+import android.widget.PopupWindow
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.treadmillassistant.R
 import com.example.treadmillassistant.backend.user
-import com.example.treadmillassistant.backend.training.TrainingStatus
 import com.example.treadmillassistant.databinding.FragmentTrainingHistoryBinding
+import com.example.treadmillassistant.databinding.MonthPickerPopupBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class TrainingHistoryFragment : Fragment() {
 
+    private var month = Calendar.getInstance().get(Calendar.MONTH)
+    private var year = Calendar.getInstance().get(Calendar.YEAR)
     private var _binding: FragmentTrainingHistoryBinding? = null
+    private var cal = Calendar.getInstance()
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -28,9 +40,10 @@ class TrainingHistoryFragment : Fragment() {
         _binding = FragmentTrainingHistoryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        updateDateButton(cal)
+
         //item adapter with training list sorted from newest to oldest as dataset
-        val itemAdapter = TrainingHistoryItemAdapter(user.trainingSchedule.trainingList.filter {
-            it.trainingStatus==TrainingStatus.Finished }.sortedByDescending { it.trainingTime })
+        var itemAdapter = TrainingHistoryItemAdapter(user.trainingSchedule.getHistoryTrainingsForMonth(cal))
 
         val linearLayoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
 
@@ -38,8 +51,45 @@ class TrainingHistoryFragment : Fragment() {
         binding.trainingHistoryTrainingList.layoutManager = linearLayoutManager
         binding.trainingHistoryTrainingList.setHasFixedSize(true)
 
+        binding.monthPickedButton.setOnClickListener {
+            val popupBinding = MonthPickerPopupBinding.inflate(layoutInflater)
+
+            val width = LinearLayout.LayoutParams.WRAP_CONTENT
+            val height = LinearLayout.LayoutParams.WRAP_CONTENT
+            val focusable = true
+            val popupWindow = PopupWindow(popupBinding.root, width, height, focusable)
+            popupWindow.contentView = popupBinding.root
+            popupBinding.datePicker.maxDate = Calendar.getInstance().timeInMillis
+            popupBinding.datePicker.updateDate(year, month, 1)
+
+            val daySpinnerId = Resources.getSystem().getIdentifier("day", "id", "android")
+            if(daySpinnerId!=0){
+                val daySpinner = popupBinding.datePicker.findViewById<NumberPicker>(daySpinnerId)
+                daySpinner.isVisible = false
+            }
+            popupWindow.showAtLocation(binding.monthPickedButton, Gravity.CENTER, 0, 0)
+
+            popupBinding.dateConfirmButton.setOnClickListener {
+                year = popupBinding.datePicker.year
+                month = popupBinding.datePicker.month
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, month)
+                itemAdapter = TrainingHistoryItemAdapter(user.trainingSchedule.getHistoryTrainingsForMonth(cal))
+                binding.trainingHistoryTrainingList.adapter = itemAdapter
+                popupWindow.dismiss()
+                updateDateButton(cal)
+            }
+        }
+
         return root
     }
+
+    private fun updateDateButton(calendar: Calendar){
+        val monthFormat = SimpleDateFormat("MMMM", Locale.US)
+        val monthName = monthFormat.format(calendar.time)
+        binding.monthPickedButton.text = String.format(getString(R.string.month_and_year), monthName, calendar.get(Calendar.YEAR).toString())
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
